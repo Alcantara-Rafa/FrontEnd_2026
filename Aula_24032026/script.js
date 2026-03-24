203c5399fc569818fc992552f3681e125c02c2f0
Rafael Alcantara Santos <rafaelalcantarasantos55@gmail.com>
2026-03-24 20:40:47 -0300

    Add files via upload

---
+++ Aula_24032026/script.js
@@ -0,0 +1,35 @@
+const criatura = document.getElementById("criatura");
+const botao = document.getElementById("botao");
+
+const estados = {
+    normal: "criatura_fofinha_manga.png",
+    puto: "criatura_brava_final.png",
+    morto: "criatura_morta_final.png",
+    comendo: "criatura_cookie_final.png",
+    alimentado: "criatura_feliz_final.png",
+}
+
+let contador = 0;
+let intervalo = null;
+let time_click = null;
+let time_out = null;
+
+function controlador(){
+    if(intervalo) clearInterval(intervalo)
+
+        intervalo = setInterval(() => {
+            contador++;
+
+            console.log("tempo:",contador);
+
+            if(contador == 30){
+                criatura.src = estados.puto
+            }
+
+            if(contador == 60){
+                criatura.src = estados.morto
+            }
+    }, 1000);
+}
+
+controlador()
